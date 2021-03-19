46581766e79e43ec0831ad5c5288e43e027a4a43
Viktar <myxabeer@gmail.com>
2021-03-20 02:58:35 +0300

    Dotfiles. Alacritty. Fish

---
+++ .xdg/config/fish/functions/launch-sway.fish
@@ -0,0 +1,7 @@
+function launch-sway --description 'Set various environment variables for Wayland compatibility and launch Sway'
+    set -x QT_QPA_PLATFORM wayland
+    set -x QT_QPA_PLATFORMTHEME qt5ct
+    set -x CLUTTER_BACKEND wayland
+
+    sway
+end
